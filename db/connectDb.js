590eba24c617924dcc1c97dbebdffa777cee5819
viveklohar229 <vishwakarmavivek521@gmail.com>
2025-04-20 18:50:56 +0530

    add text-tb folter to check mongo

--- db/connectDb.js
+++ db/connectDb.js
@@ -3,10 +3,9 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
         try {
-            const conn = await mongoose.connect(process.env.MONGO_URI, {
-                useNewUrlParser: true,
-            });
+            const conn = await mongoose.connect(process.env.MONGO_URI);
             console.log(`MongoDB Connected: ${conn.connection.host}`);
+            console.log(process.env.MONGO_URI)
             return conn;
             
         } catch (error) {
